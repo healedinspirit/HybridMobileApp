163092e4f75831b434bcb438b8e250e3e1bbbf7c
Eric Jones <seeriuss@gmail.com>
2018-06-13 15:14:54 -0600

    Monitoring on home page.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,12 +8,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { AnalyticsService } from '../../services/analytics-service';
 import { SocialSharing } from '@ionic-native/social-sharing';
 import { GlobalService } from '../../services/global.service';
+import { Pro } from '@ionic/pro';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private pro = Pro.init('f9fd3452', { appVersion: '2.18.6' });
 
   constructor(public navCtrl: NavController,
     private ga: AnalyticsService,
@@ -47,7 +49,7 @@ export class HomePage {
   
     }
     catch (e) {
-      this.global.addLogItem(e);
+      this.pro.monitoring.exception(new Error('Error in Home.Share(): ' + e));
     }
   }
 
@@ -57,7 +59,7 @@ export class HomePage {
       this.sharing.share('Check it out.', null, null, 'www.healedinspirit.com');
     }
     catch (e) {
-      this.global.addLogItem(e);
+      this.pro.monitoring.exception(new Error('Error in Home.ShareToFacebook(): ' + e));
     }
   }
 
@@ -67,7 +69,7 @@ export class HomePage {
         this.sharing.share('Check this app!', null, null, null);
       }
       catch (e) {
-        this.global.addLogItem(e);
+        this.pro.monitoring.exception(new Error('Error in Home.ShareToTwitter(): ' + e));
       }
   }
 
@@ -77,7 +79,7 @@ export class HomePage {
       this.sharing.share('Check this app!', null, null, null);
     }
     catch (e) {
-      this.global.addLogItem(e);
+      this.pro.monitoring.exception(new Error('Error in Home.ShareToEmail(): ' + e));
     }
   }
 
